759adee09172f6de82017418a299842f2b1fe272
georgijs.kadolciks <georgijs.kadolciks@accenture.com>
2022-12-28 20:15:11 +0200

    Fix Task 3

--- src/MyBubbleSort.java
+++ src/MyBubbleSort.java
@@ -5,14 +5,18 @@ public class MyBubbleSort {
         int topIndex = array.length - 1;
         if (topIndex < 1) return;
         int swapBuffer;
+        boolean isSorted;
         for (int sortedPortionLimit = 0; sortedPortionLimit < topIndex; sortedPortionLimit++) {
+            isSorted = true;
             for (int i = topIndex - 1; i >= sortedPortionLimit; i--) {
                 if (array[i] > array[i + 1]) {
                     swapBuffer = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = swapBuffer;
+                    if (isSorted) isSorted = false;
                 }
             }
+            if (isSorted) return;
         }
     }
 }
